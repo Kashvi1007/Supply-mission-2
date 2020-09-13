075962b9bfe60d1ddc155cfb5f4fc4d54179a6c1
Kashvi1007 <68150265+Kashvi1007@users.noreply.github.com>
2020-09-13 15:04:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
-var ground, helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,rightSpr,liftSpr,midSpr; 
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var rect1,rect2,rect3;
+
 const Engine = Matter.Engine;
-const World = Matter.World; 
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
@@ -15,44 +17,33 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.scale=0.6;
+
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color("green");
 
-	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
 
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	rightSpr=createSprite(510, 610, 20, 100);
-	rightSpr.shapeColor=color("red");
-
-	liftSpr=createSprite(290, 610, 20,100);
-	liftSpr.shapeColor=color("red");
-	
-	midSpr=createSprite(400, 650, 200, 20);
-	midSpr.shapeColor=color("red");
-   
 	engine = Engine.create();
 	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
 	
+
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	World.add(world, ground);
+	 
+	rect1 = new Box(400,650,200,20);
+	rect2 = new Box(300,610,20,100);
+	rect3 = new Box(500,610,20,100);
 
-	packageBody = Bodies.circle(width/2 , 200 , 23 , {restitution: 0.1, isStatic:true});
-	World.add(world, packageBody);
-	
-	rightBod = Bodies.rectangle(500, 610, 20, 100, {isStatic:true});
-	World.add(world, rightBod);
-
-	liftBod = Bodies.rectangle(300, 610, 20, 100, {isStatic:true});
-	World.add(world, liftBod);
-	
-	midBod = Bodies.rectangle(400, 650, 200, 20, {isStatic:true});
-	World.add(world, midBod);
 
 	Engine.run(engine);
   
@@ -61,31 +52,23 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  ellipseMode(CENTER); 
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+	background(0);
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y - 10;
 
-  rightSpr.x= rightBod.position.x 
-  rightSpr.y= rightBod.position.y 
+  rect1.display();
+  rect2.display();
+  rect3.display();
 
-  liftSpr.x= liftBod.position.x 
-  liftSpr.y= liftBod.position.y 
+  keyPressed();
 
-  midSpr.x= midBod.position.x 
-  midSpr.y= midBod.position.y 
   drawSprites();
-
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-	Body.setStatic(packageBody, false);
-
+	Matter.Body.setStatic(packageBody, false);
   }
 }
-
-
-
